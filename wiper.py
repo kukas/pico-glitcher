3f0a8b54da1d3808bd437af35c65db4b975f4192
Jirka Balhar <balhar.j@gmail.com>
2024-04-23 09:29:59 +0200

    fix wiper.py

--- wiper.py
+++ wiper.py
@@ -12,8 +12,15 @@ with serial.Serial('/dev/ttyACM0', 115200) as ser:
     print(response)
     print(f"debug locked: {response[3] & (1<<2)}")
     
+    sure = input("Are you sure you want to wipe the board? (y/n): ")
+    if sure != 'y':
+        sys.exit(0)
+
     ser.write(b'p')
-    ser.write(struct.pack('>Q', 1000))
+    # send the password (defined in main.cpp)
+    ser.write(struct.pack('>Q', 0xAABBCCDDEEFF0102))
+    # set the delay after reset (in us)
+    ser.write(struct.pack('>Q', 1_000_000))
     response = ser.read(1)
     print(response)
     print(f"debug locked: {response[0] & 0x04}")
